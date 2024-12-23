f4d660851bc0a4cf5a89f632230a38d7e80dac9f
luisrico13 <luisrico13@proton.me>
2024-12-23 00:30:08 +0000

    ejercicio listo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Home</Link>
+        <div className="ml-auto">
+          <Link className="btn btn-primary me-2" to="/contacts">Contacts</Link>
+          <Link className="btn btn-secondary" to="/add-contact">Add Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
